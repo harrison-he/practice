69e4228baa2dbaa37cf373c5b353bf2d13ced3d2
Harrison He <harrisonhe1@gmail.com>
2019-08-17 21:17:35 -0400

    Added Server file with HTTP Server

--- public/index.js
+++ public/index.js
@@ -5,13 +5,20 @@ const toDoItems = ["Example To Do", "Example To Do 2"];
 const addToDoItem = event => {
     event.preventDefault();
     const value = event.target[0].value;
-    if (value != "") {
+    if (value) {
         toDoItems.push(value);
 
-
         const node = document.createElement("LI");
-        const createTextNode = document.createTextNode(value);
-        node.appendChild(createTextNode);
+        node.innerHTML = value;
+        // const textNode = document.createTextNode(`${value} `);
+        // node.appendChild(textNode);
+
+        // const buttonNode = document.createElement("BUTTON");
+        // const buttonTextNode = document.createTextNode("X");
+
+        // buttonNode.appendChild(buttonTextNode);
+        // node.appendChild(buttonNode);
+
         document.getElementById("toDoList").appendChild(node);
         document.getElementById("addToDo").value = "";
     }
